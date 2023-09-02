a7f58dfafaa16cd580192c29c91528e20e394dd7
kandula Thrinath <k.thrinath2227@gmail.com>
2023-09-02 17:35:36 +0530

    bootstrap login page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <div class="row">
+    <div :class="'col-lg-6 col-md-8 col-sm-10'">
+      <div class="bg-container">
+          <div class="white-bg-container">
+            <img src="../src/assets/AAQ-1904.png" alt="Logo" class="logo"/>
+          </div>
+          
+      </div>
+    </div>
   </div>
+      
+</div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+ 
+  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  padding:0px;
+  margin:0px
 }
+.row {
+  display: flex;
+  justify-content: center;
+}
+.bg-container {
+  background-image: url('../src/assets/Artboard 1.png');
+  background-color: rgba(15,7,128,0.973);
+  height:99vh;
+  background-size: cover;
+  width:auto;
+
+  background-position: top;
+}
+@media (max-width: 1000px) {
+
+.bg-container {
+    background-size: 1000px px; /* Force the image to its minimum width */
+}
+}
+.white-bg-container {
+  background-image: url('../src/assets/Path 34333.svg');
+  background-size: cover;
+  height: auto;
+  width: 100%;
+  background-position: top;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding:5px;
+}
+.logo {
+  width:160px;
+  height:auto;
+  margin: 5px;
+}
+
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,28 @@
+
+
+
+
 import Vue from 'vue'
 import App from './App.vue'
 
+
+
+/*     fontawesome */
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
+
+
+
+/* 
+Bootstap 
+
+*/
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
